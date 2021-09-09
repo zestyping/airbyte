65026baecede7bb4140203c33fedaabe61042dcc
Ka-Ping Yee <zestyping@gmail.com>
2021-09-09 03:12:08 -0700

    Make separate requests for each email message in order to retrieve its content.

--- airbyte-integrations/connectors/source-ngpvan/source_ngpvan/source.py
+++ airbyte-integrations/connectors/source-ngpvan/source_ngpvan/source.py
@@ -38,11 +38,34 @@ class NgpvanStream(HttpStream, ABC):
 
 class EmailMessages(NgpvanStream):
     primary_key = 'foreignMessageId'
+    cursor_field = 'dateModified'
+    state_checkpoint_interval = 100
+
+    def __init__(self, authenticator, min_date: str = None):
+        super().__init__(authenticator=authenticator)
+        self.min_date = min_date
+
+    def get_updated_state(self, stream_state, latest_record):
+        return max(stream_state, latest_record['dateModified'])
 
     def path(self, stream_state, stream_slice, next_page_token):
         if next_page_token:
             return '/email/messages?' +  next_page_token.split('?', 1)[1]
         return '/email/messages'
+
+    def request_params(self, stream_state, stream_slice, next_page_token):
+        return {'$top': '10', '$orderby': 'dateModified asc'}
+
+    def parse_response(self, response, **kwargs) -> Iterable[Mapping]:
+        for item in response.json()['items']:
+            item_request = self._create_prepared_request(
+                path='/email/message/' + item['foreignMessageId'],
+                headers=self.authenticator.get_auth_header(),
+                params={'$expand': 'emailMessageContent,emailMessageContentDistributions'}
+            )
+            item_kwargs = self.request_kwargs(**kwargs)
+            item_response = self._send_request(item_request, item_kwargs)
+            yield item_response.json()
 
     def get_json_schema(self):
         STRING_TYPE = {'type': 'string'}
@@ -88,71 +111,12 @@ class EmailMessages(NgpvanStream):
         }
 
 
-class IncrementalNgpvanStream(NgpvanStream, ABC):
-    """
-    TODO fill in details of this class to implement functionality related to incremental syncs for your connector.
-         if you do not need to implement incremental sync for any streams, remove this class.
-    """
-
-    # TODO: Fill in to checkpoint stream reads after N records. This prevents re-reading of data if the stream fails for any reason.
-    state_checkpoint_interval = None
-
-    @property
-    def cursor_field(self) -> str:
-        """
-        TODO
-        Override to return the cursor field used by this stream e.g: an API entity might always use created_at as the cursor field. This is
-        usually id or date based. This field's presence tells the framework this in an incremental stream. Required for incremental.
-
-        :return str: The name of the cursor field.
-        """
-        return []
-
-    def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> Mapping[str, Any]:
-        """
-        Override to determine the latest state after reading the latest record. This typically compared the cursor_field from the latest record and
-        the current state and picks the 'most' recent cursor. This is how a stream's state is determined. Required for incremental.
-        """
-        return {}
-
-
-class IncrementalEmailMessages(IncrementalNgpvanStream):
-    primary_key = 'foreignMessageId'
-    cursor_field = 'dateModified'
-
-    def path(self, stream_state, stream_slice, next_page_token):
-        return "/email/messages"
-
-    def stream_slices(self, stream_state: Mapping[str, Any] = None, **kwargs) -> Iterable[Optional[Mapping[str, any]]]:
-        """
-        TODO: Optionally override this method to define this stream's slices. If slicing is not needed, delete this method.
-
-        Slices control when state is saved. Specifically, state is saved after a slice has been fully read.
-        This is useful if the API offers reads by groups or filters, and can be paired with the state object to make reads efficient. See the "concepts"
-        section of the docs for more information.
-
-        The function is called before reading any records in a stream. It returns an Iterable of dicts, each containing the
-        necessary data to craft a request for a slice. The stream state is usually referenced to determine what slices need to be created.
-        This means that data in a slice is usually closely related to a stream's cursor_field and stream_state.
-
-        An HTTP request is made for each returned slice. The same slice can be accessed in the path, request_params and request_header functions to help
-        craft that specific request.
-
-        For example, if https://example-api.com/v1/employees offers a date query params that returns data for that particular day, one way to implement
-        this would be to consult the stream state object for the last synced date, then return a slice containing each date from the last synced date
-        till now. The request_params function would then grab the date from the stream_slice and make it part of the request by injecting it into
-        the date query param.
-        """
-        raise NotImplementedError("Implement stream slices or delete this method!")
-
-
 def get_authenticator(config):
     username = 'apiuser'
     password = f'{config["van_api_key"]}|{config["database_mode"]}'
     return BasicAuthenticator(username, password)
 
 
-# Source
 class SourceNgpvan(AbstractSource):
     def check_connection(self, logger, config) -> Tuple[bool, any]:
         try:
